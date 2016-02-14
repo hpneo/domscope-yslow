c1635015be1ba3c56bf580d75df496fa5fe5c906
HPNeo <hpneo@hotmail.com>
2016-02-14 01:17:05 -0500

    Add options

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express'),
     exec = require('child_process').exec,
+    pick = require('lodash-node/modern/object/pick'),
+    reduce = require('lodash-node/modern/collection/reduce'),
     port = process.env.PORT || 3000;
 
 var app = express();
 
 app.get('/', function(request, response) {
-  var url = request.query.url;
+  var url = request.query.url,
+      options = pick(request.query, ['info', 'ruleset', 'threshold', 'ua', 'viewport', 'headers', 'cdns']),
+      optionsAsArguments = reduce(options, function(result, value, key) {
+        if (key === 'ua') {
+          value = '"' + value + '"';
+        }
 
-  exec('phantomjs ./yslow.js ' + url, function(error, stdout, stderr) {
+        result.push('--' + key + ' ' + value);
+        return result;
+      }, []);
+
+
+  exec('phantomjs ./yslow.js --dict ' + optionsAsArguments + ' ' + url, function(error, stdout, stderr) {
     if (error === null) {
       response.json(JSON.parse(stdout));
     }
